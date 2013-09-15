aff1f650d3c8de5c9a23a04dc89bc5429c4e614a
gbathree <gbathree@gmail.com>
2013-09-15 13:54:45 -0400

    Further updates to JSON names passed from device to web to app

--- spec/controllers/projects_spec.rb
+++ spec/controllers/projects_spec.rb
@@ -36,7 +36,7 @@ describe ProjectsController do
     describe "POST #contribute" do
         it "posts data to a contributing project successfully" do
           project = create(:project, :lead_id => @user.id,:is_open=>false)
-          project_data = {:co2_content=>"4.0",:created_at=>"2013-09-04T10:45:09Z",:photosynthetic_efficiency_phi2=>"24.0",:phi2_raw=>"24,421,124",:relative_chlorophyll_content=>"324.0",:relative_humidity=>"4.0",:tags=>"Apple",:temperature=>"44.0",:baseline=>"20.31",:co2_raw=>"12,32,51",:co2_slope=>"-.82",:co2_drop=>"450"}
+          project_data = {:version=>"1",:co2_content=>"4.0",:created_at=>"2013-09-04T10:45:09Z",:photosynthetic_efficiency_phi2=>"24.0",:phi2_raw=>"24,421,124",:relative_chlorophyll_content_fs=>"23.2",:fm=>"55.4",:relative_humidity=>"4.0",:tags=>"Apple",:temperature=>"44.0",:baseline=>"20.31",:co2_raw=>"12,32,51",:co2_slope=>"-.82",:co2_start=>"850",:co2_end=>"450",:co2_drop=>".50",:light_intensity=>"120",:blue=>"102",:green=>"32",:red=>"12",:led_low=>"42",:led_high=>"76",:ir_low=>"76",:ir_high=>"109"}
           collaborator = create(:confirmed_user)
           collaborator.reset_authentication_token! 
           ProjectCollaborator.create(:project_id=>project.id,:user_id=>collaborator.id)
